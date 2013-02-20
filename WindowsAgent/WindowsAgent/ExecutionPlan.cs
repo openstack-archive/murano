d141fbc4dc313f1481785afce1217fcb28457c9b
Stan Lagun <slagun@mirantis.com>
2013-02-20 23:15:44 +0400

    [KEERO-83] Windows Agent initial implementation

---
+++ WindowsAgent/WindowsAgent/ExecutionPlan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirantis.Keero.WindowsAgent
+{
+	class ExecutionPlan
+	{
+		public class Command
+		{
+			public string Name { get; set; } 
+			public Dictionary<string, object> Arguments { get; set; }
+		}
+
+		public string[] Scripts { get; set; }
+		public LinkedList<Command> Commands { get; set; }
+	}
+}
---
+++ WindowsAgent/WindowsAgent/MqMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirantis.Keero.WindowsAgent
+{
+	class MqMessage
+	{
+		private readonly Action ackFunc;
+
+		public MqMessage(Action ackFunc)
+		{
+			this.ackFunc = ackFunc;
+		}
+
+		public string Body { get; set; }
+
+		public void Ack()
+		{
+			ackFunc();
+		}
+	}
+}
