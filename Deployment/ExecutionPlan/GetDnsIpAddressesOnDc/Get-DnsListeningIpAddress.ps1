11316041444c6193d6e8aa1b3870aacbb9ec1af1
Dmitry Teselkin <dteselkin@mirantis.com>
2013-03-10 15:16:54 +0400

    Typo

--- Deployment/ExecutionPlan/GetDnsIpAddressesOnDc/Get-DnsListeningIpAddress.ps1
+++ Deployment/ExecutionPlan/GetDnsIpAddressesOnDc/Get-DnsListeningIpAddress.ps1
@@ -1,4 +1,4 @@
-function Get-DnsListeningIpAddresses {
+function Get-DnsListeningIpAddress {
     Import-Module DnsServer
     (Get-DNSServer -ComputerName localhost).ServerSetting.ListeningIpAddress
 }
