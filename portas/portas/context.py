96b90202fe570177c0c4a6c61766de1a059e88b3
Serg Melikyan <smelikyan@mirantis.com>
2013-03-27 18:45:30 +0400

    Fix name of the variable

--- portas/portas/context.py
+++ portas/portas/context.py
@@ -24,10 +24,10 @@ class RequestContext(object):
     accesses the system, as well as additional request information.
     """
 
-    def __init__(self, auth_tok=None, user=None, tenant=None,
+    def __init__(self, auth_token=None, user=None, tenant=None,
                  roles=None, service_catalog=None, session=None):
 
-        self.auth_tok = auth_tok
+        self.auth_token = auth_token
         self.user = user
         self.tenant = tenant
         self.roles = roles or []
@@ -51,7 +51,7 @@ class RequestContext(object):
             'project_id': self.tenant,
 
             'roles': self.roles,
-            'auth_token': self.auth_tok,
+            'auth_token': self.auth_token,
             'session': self.session
         }
 
