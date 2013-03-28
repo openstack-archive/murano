e6801a9edde3e8d6b27102341053fb79d8fbfd4c
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-03-28 17:21:08 +0400

    Experiments.

--- tabula/tabula/windc/api.py
+++ tabula/tabula/windc/api.py
@@ -90,6 +90,11 @@ def services_list(request, datacenter_id):
     services = windcclient(request).activeDirectories.list(datacenter_id,
                                                            session_id)
     services += windcclient(request).webServers.list(datacenter_id, session_id)
+    
+   
+    for i in range(len(services)):
+        services[i].operation = windcclient(request).sessions.\
+                                reports(datacenter_id, session_id)
 
     return services
 
