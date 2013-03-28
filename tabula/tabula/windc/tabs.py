7022ce82891a85d51165f4710e7b941a66e4cf52
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-03-28 17:13:58 +0400

    Experiments.

--- tabula/tabula/windc/tabs.py
+++ tabula/tabula/windc/tabs.py
@@ -45,10 +45,10 @@ class LogsTab(tabs.Tab):
     
     def get_context_data(self, request):
         data = self.tab_group.kwargs['service']
+        
+        reports = api.get_status_message_for_service(request, data.id)
 
-        LOG.critical(self.tab_group.kwargs)
-
-        return {"service": data}
+        return {"reports": reports}
 
 
 class WinServicesTabs(tabs.TabGroup):
