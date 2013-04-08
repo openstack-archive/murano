6765719da90f8e1b762cd3edbf8296b37d879d40
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-04-08 12:44:02 +0400

    Fixed small issue with new names.

--- dashboard/manage.py
+++ dashboard/manage.py
@@ -5,6 +5,6 @@ import os
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tabula.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "glazierdashboard.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
