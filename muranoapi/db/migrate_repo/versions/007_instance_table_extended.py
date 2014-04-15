6291c859401ddbbae4701623935759b7b6b69c70
Stan Lagun <slagun@mirantis.com>
2014-04-15 18:32:09 +0400

    Fixes DB migrations on SQLite
    Change-Id: I07960799adf07ad39582f90feba27a84979aea76
    Closes-Bug: #1308089

--- muranoapi/db/migrate_repo/versions/007_instance_table_extended.py
+++ muranoapi/db/migrate_repo/versions/007_instance_table_extended.py
@@ -21,16 +21,20 @@ meta = schema.MetaData()
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
     table = schema.Table('instance', meta, autoload=True)
-    table.rename('instance_stats')
-    table.delete()
-    type_name = schema.Column('type_name', types.String(512), nullable=False)
-    type_name.create(table)
-    type_title = schema.Column('type_title', types.String(512))
-    type_title.create(table)
-    unit_count = schema.Column('unit_count', types.Integer())
-    unit_count.create(table)
-    tenant_id = schema.Column('tenant_id', types.String(32), nullable=False)
-    tenant_id.create(table)
+    table.drop()
+    table = schema.Table(
+        'instance_stats',
+        meta,
+        schema.Column('environment_id', types.String(100), primary_key=True),
+        schema.Column('instance_id', types.String(100), primary_key=True),
+        schema.Column('instance_type', types.Integer, nullable=False),
+        schema.Column('created', types.Integer, nullable=False),
+        schema.Column('destroyed', types.Integer, nullable=True),
+        schema.Column('type_name', types.String(512), nullable=False),
+        schema.Column('type_title', types.String(512)),
+        schema.Column('unit_count', types.Integer()),
+        schema.Column('tenant_id', types.String(32), nullable=False))
+    table.create()
 
 
 def downgrade(migrate_engine):
