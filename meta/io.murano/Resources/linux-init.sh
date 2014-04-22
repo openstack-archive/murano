ca9aae15549d1941dfb78fc6ba7be5e6703e25f3
Serg Melikyan <smelikyan@mirantis.com>
2014-04-22 18:45:55 +0400

    Fixed name of the log file on VM
    Small fix for images with pre-created /etc/murano directory
    
    Change-Id: Ie5738b89efba7619f0d4e30500407ff0daea4f6a

--- meta/io.murano/Resources/linux-init.sh
+++ meta/io.murano/Resources/linux-init.sh
@@ -4,7 +4,9 @@ service murano-agent stop
 
 AgentConfigBase64='%AGENT_CONFIG_BASE64%'
 
-mkdir /etc/murano
+if [[ ! -d /etc/murano ]]; then
+    mkdir /etc/murano
+fi
 echo $AgentConfigBase64 | base64 -d > /etc/murano/agent.conf
 chmod 664 /etc/murano/agent.conf
 
