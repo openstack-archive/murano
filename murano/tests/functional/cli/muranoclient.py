efd90a703611f9f1a22ec3efb32809e8316739fe
Sergey Murashov <smurashov@mirantis.com>
2014-07-15 16:53:31 +0400

    Add simple smoke tests for Murano CLI client
    Add the following tests:
    - Check environment list
    - Check package list
    - Check category list
    
    Change-Id: Ib29f3e555ed79c5c7350cac0333e0473e91ab389
    Implements: blueprint cli-simple-read-only-tests

---
+++ murano/tests/functional/cli/muranoclient.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2014 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tempest import cli
+
+
+class ClientTestBase(cli.ClientTestBase):
+
+    def murano(self, action, flags='', params='', admin=True, fail_ok=False):
+        """Executes murano command for the given action."""
+        return self.cmd_with_auth(
+            'murano', action, flags, params, admin, fail_ok)
