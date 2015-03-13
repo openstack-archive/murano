7d2b0279f918d6a3c05a798ae5c4263e330aa42d
Dmitry Teselkin <dteselkin@mirantis.com>
2015-03-13 11:00:44 +0300

    Use python from venv for subunit-trace
    If subunit installed from packages then subunit-trace goes to
    /usr/bin/ dir with hasbang string #!/usr/bin/python
    
    In this case even if called from venv it use system python
    interpreter, and will not have access to modules from venv.
    
    Change-Id: I9b152055c5ce958001e59908564a3549960a7df9

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -3,4 +3,4 @@
 set -o pipefail
 
 TESTRARGS=$1
-python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
+python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | python $(which subunit-trace) -f
