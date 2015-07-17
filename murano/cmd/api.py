1d275dfb973380f2de956f326c0ad9f27737aaee
Nikolay Starodubtsev <nstarodubtsev@mirantis.com>
2015-07-17 14:10:13 +0300

    Separate load_app from api starter
    Move load_app function to a separate file in murano/common. This
    change needed for future Cloud Foundry Service Broker API
    implementation.
    
    Change-Id: I3a3f432ea82b0b52f132eabb83a6856e27aef625

--- murano/cmd/api.py
+++ murano/cmd/api.py
@@ -34,93 +34,17 @@ if os.path.exists(os.path.join(root, 'murano', '__init__.py')):
     sys.path.insert(0, root)
 
 from oslo_config import cfg
-from oslo_log import log as logging
 from oslo_service import service
 
-# It appears that with the pylint 1.2 release one can not
-# check packages that import from namespace packages anymore,
-# so using this workaround to solve the problem
-import paste.deploy as deploy
-
 from murano.api.v1 import request_statistics
+from murano.common import app_loader
 from murano.common import config
-from murano.common.i18n import _
 from murano.common import policy
 from murano.common import server
 from murano.common import statservice as stats
 from murano.common import wsgi
 
 CONF = cfg.CONF
-
-
-def _get_deployment_flavor():
-    """Retrieve the paste_deploy.flavor config item, formatted appropriately
-       for appending to the application name.
-    """
-    flavor = CONF.paste_deploy.flavor
-    return '' if not flavor else ('-' + flavor)
-
-
-def _get_paste_config_path():
-    paste_suffix = '-paste.ini'
-    conf_suffix = '.conf'
-    if CONF.config_file:
-        # Assume paste config is in a paste.ini file corresponding
-        # to the last config file
-        path = CONF.config_file[-1].replace(conf_suffix, paste_suffix)
-    else:
-        path = CONF.prog + '-paste.ini'
-    return CONF.find_file(os.path.basename(path))
-
-
-def _get_deployment_config_file():
-    """Retrieve the deployment_config_file config item, formatted as an
-       absolute pathname.
-    """
-    path = CONF.paste_deploy.config_file
-    if not path:
-        path = _get_paste_config_path()
-    if not path:
-        msg = _("Unable to locate paste config file for %s.") % CONF.prog
-        raise RuntimeError(msg)
-    return os.path.abspath(path)
-
-
-def load_paste_app(app_name=None):
-    """Builds and returns a WSGI app from a paste config file.
-
-    We assume the last config file specified in the supplied ConfigOpts
-    object is the paste config file.
-
-    :param app_name: name of the application to load
-
-    :raises RuntimeError when config file cannot be located or application
-            cannot be loaded from config file
-    """
-    if app_name is None:
-        app_name = CONF.prog
-
-    # append the deployment flavor to the application name,
-    # in order to identify the appropriate paste pipeline
-    app_name += _get_deployment_flavor()
-
-    conf_file = _get_deployment_config_file()
-
-    try:
-        logger = logging.getLogger(__name__)
-        logger.debug("Loading %(app_name)s from %(conf_file)s".format(
-            conf_file=conf_file, app_name=app_name))
-
-        app = deploy.loadapp("config:%s" % conf_file, name=app_name)
-
-        return app
-    except (LookupError, ImportError) as e:
-        msg = _("Unable to load %(app_name)s from configuration file"
-                " %(conf_file)s. \nGot: %(e)r") % {'conf_file': conf_file,
-                                                   'app_name': app_name,
-                                                   'e': e}
-        logger.error(msg)
-        raise RuntimeError(msg)
 
 
 def main():
@@ -131,10 +55,11 @@ def main():
 
         launcher = service.ServiceLauncher(CONF)
 
-        app = load_paste_app('murano')
+        app = app_loader.load_paste_app('murano')
         port, host = (CONF.bind_port, CONF.bind_host)
 
         launcher.launch_service(wsgi.Service(app, port, host))
+
         launcher.launch_service(server.get_rpc_service())
         launcher.launch_service(server.get_notification_service())
         launcher.launch_service(stats.StatsCollectingService())
---
+++ murano/common/app_loader.py
@@ -0,0 +1,94 @@
+#    Copyright (c) 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import os
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from paste import deploy
+
+from murano.common.i18n import _
+
+CONF = cfg.CONF
+
+
+def _get_deployment_flavor():
+    """Retrieve the paste_deploy.flavor config item, formatted appropriately
+       for appending to the application name.
+    """
+    flavor = CONF.paste_deploy.flavor
+    return '' if not flavor else ('-' + flavor)
+
+
+def _get_paste_config_path():
+    paste_suffix = '-paste.ini'
+    conf_suffix = '.conf'
+    if CONF.config_file:
+        # Assume paste config is in a paste.ini file corresponding
+        # to the last config file
+        path = CONF.config_file[-1].replace(conf_suffix, paste_suffix)
+    else:
+        path = CONF.prog + '-paste.ini'
+    return CONF.find_file(os.path.basename(path))
+
+
+def _get_deployment_config_file():
+    """Retrieve the deployment_config_file config item, formatted as an
+       absolute pathname.
+    """
+    path = CONF.paste_deploy.config_file
+    if not path:
+        path = _get_paste_config_path()
+    if not path:
+        msg = _("Unable to locate paste config file for %s.") % CONF.prog
+        raise RuntimeError(msg)
+    return os.path.abspath(path)
+
+
+def load_paste_app(app_name=None):
+    """Builds and returns a WSGI app from a paste config file.
+
+    We assume the last config file specified in the supplied ConfigOpts
+    object is the paste config file.
+
+    :param app_name: name of the application to load
+
+    :raises RuntimeError when config file cannot be located or application
+            cannot be loaded from config file
+    """
+    if app_name is None:
+        app_name = CONF.prog
+
+    # append the deployment flavor to the application name,
+    # in order to identify the appropriate paste pipeline
+    app_name += _get_deployment_flavor()
+
+    conf_file = _get_deployment_config_file()
+
+    try:
+        logger = logging.getLogger(__name__)
+        logger.debug("Loading %(app_name)s from %(conf_file)s".format(
+            conf_file=conf_file, app_name=app_name))
+
+        app = deploy.loadapp("config:%s" % conf_file, name=app_name)
+
+        return app
+    except (LookupError, ImportError) as e:
+        msg = _("Unable to load %(app_name)s from configuration file"
+                " %(conf_file)s. \nGot: %(e)r") % {'conf_file': conf_file,
+                                                   'app_name': app_name,
+                                                   'e': e}
+        logger.error(msg)
+        raise RuntimeError(msg)
