24cdb64a1e4c301ba8cb61c348f7cd2756331ced
Kirill Zaitsev <kzaitsev@mirantis.com>
2015-09-25 16:17:56 +0300

    Enable H233: Python 3.x incompatible use of print operator
    This commit removes H233 from ignored list and fixes corresponding code
    to pass the check.
    
    Change-Id: I5f842e74e5c05f421307e29c059d63b309f1d23d

--- murano/common/wsgi.py
+++ murano/common/wsgi.py
@@ -206,16 +206,16 @@ class Debug(Middleware):
 
     @webob.dec.wsgify
     def __call__(self, req):
-        print ("*" * 40) + " REQUEST ENVIRON"
+        print(("*" * 40) + " REQUEST ENVIRON")
         for key, value in req.environ.items():
             print(key, "=", value)
-        print
+        print("")
         resp = req.get_response(self.application)
 
-        print ("*" * 40) + " RESPONSE HEADERS"
+        print(("*" * 40) + " RESPONSE HEADERS")
         for (key, value) in resp.headers.iteritems():
             print(key, "=", value)
-        print
+        print("")
 
         resp.app_iter = self.print_generator(resp.app_iter)
 
@@ -226,12 +226,12 @@ class Debug(Middleware):
         """Iterator that prints the contents of a wrapper string iterator
         when iterated.
         """
-        print ("*" * 40) + " BODY"
+        print(("*" * 40) + " BODY")
         for part in app_iter:
             sys.stdout.write(part)
             sys.stdout.flush()
             yield part
-        print
+        print("")
 
 
 class Router(object):
