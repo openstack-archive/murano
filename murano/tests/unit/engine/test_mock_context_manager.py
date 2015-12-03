d32175746810a22b551e0d8aa3dd74c9040c642e
Ekaterina Chernova <efedorova@mirantis.com>
2015-12-03 17:55:41 +0300

    [mocking-machinery] Add inject YAQL functions
    Functions for injecting class methods or objects with mocks are added:
    * mock can be provided as method name
    * mock can be provided as YAQL expression
    
    * Also, withOriginal function is added.
    
      withOriginal allows to use data from the original context in mock function.
    
    Targets blueprint mock-context-manager
    
    Depends-On: I9a55d07188ff06bdf98248f011a700c297e33417
    
    Change-Id: Iefee767390209bfad0cca4f39389e0205cd3c9e2

--- murano/tests/unit/engine/test_mock_context_manager.py
+++ murano/tests/unit/engine/test_mock_context_manager.py
@@ -14,9 +14,16 @@ import mock
 from yaql import contexts
 from yaql import specs
 
+from murano.dsl import constants
+from murano.dsl import executor
 from murano.dsl import murano_class
+from murano.engine import environment
 from murano.engine import mock_context_manager
 from murano.tests.unit import base
+from murano.tests.unit.dsl.foundation import object_model as om
+from murano.tests.unit.dsl.foundation import runner
+from murano.tests.unit.dsl.foundation import test_case
+
 
 FIXTURE_CLASS = 'io.murano.system.Agent'
 FIXTURE_FUNC = 'call'
@@ -24,6 +31,34 @@ FIXTURE_FUNC = 'call'
 
 def _get_fd(set_to_extract):
     return list(set_to_extract)[0]
+
+
+class TestMockContextManager(mock_context_manager.MockContextManager):
+    def __init__(self, functions):
+        super(TestMockContextManager, self).__init__()
+        self.__functions = functions
+
+    def create_root_context(self, runtime_version):
+        root_context = super(TestMockContextManager, self).create_root_context(
+            runtime_version)
+        context = root_context.create_child_context()
+        for name, func in self.__functions.iteritems():
+            context.register_function(func, name)
+        return context
+
+
+class MockRunner(runner.Runner):
+    def __init__(self, model, package_loader, functions):
+        if isinstance(model, basestring):
+            model = om.Object(model)
+        model = om.build_model(model)
+        if 'Objects' not in model:
+            model = {'Objects': model}
+
+        self.executor = executor.MuranoDslExecutor(
+            package_loader, TestMockContextManager(functions),
+            environment.Environment())
+        self._root = self.executor.load(model).object
 
 
 class TestMockManager(base.MuranoTestCase):
@@ -54,3 +89,41 @@ class TestMockManager(base.MuranoTestCase):
         # Mock function should go first, but result context should contain both
         self.assertIs(mock_function, _get_fd(all_functions[0]))
         self.assertIs(original_function, _get_fd(all_functions[1]))
+
+    def test_create_root_context(self):
+        mock_manager = mock_context_manager.MockContextManager()
+        ctx_to_check = mock_manager.create_root_context(
+            constants.RUNTIME_VERSION_1_1)
+        inject_count = ctx_to_check.collect_functions('inject')
+        with_original_count = ctx_to_check.collect_functions('withOriginal')
+
+        self.assertEqual(2, len(inject_count[0]))
+        self.assertEqual(1, len(with_original_count[0]))
+
+
+class TestMockYaqlFunctions(test_case.DslTestCase):
+
+    def setUp(self):
+        super(TestMockYaqlFunctions, self).setUp()
+        self.runner = MockRunner(om.Object('TestMocks'),
+                                 self.package_loader, self._functions)
+
+    def test_inject_method_with_str(self):
+        self.runner.testInjectMethodWithString()
+        self.assertEqual(['This is mock1'], self.traces)
+
+    def test_inject_object_with_str(self):
+        self.runner.testInjectObjectWithString()
+        self.assertEqual(['This is mock1'], self.traces)
+
+    def test_inject_method_with_yaql_expr(self):
+        self.runner.testInjectMethodWithYaqlExpr()
+        self.assertEqual(['method1', 'I am mock'], self.traces)
+
+    def test_inject_object_with_yaql_expr(self):
+        self.runner.testInjectObjectWithYaqlExpr()
+        self.assertEqual(['method1', 'I am mock'], self.traces)
+
+    def test_with_original(self):
+        self.runner.testWithoriginal()
+        self.assertEqual(['DEFAULT'], self.traces)
