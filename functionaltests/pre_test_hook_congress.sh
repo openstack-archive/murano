f215c047ed22c4c285968d31c111b09b1bc0337e
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2015-12-10 16:21:29 +0300

    Remove libs and libs usage from murano and murano hooks
    - Remove devstack libs
    - Remove libs usage in congress hooks
    
    Change-Id: Ieeb76dcbe0ab953dba156413af0906bcd8cb6b45
    Closes-Bug: #1508165

--- functionaltests/pre_test_hook_congress.sh
+++ functionaltests/pre_test_hook_congress.sh
@@ -3,6 +3,4 @@
 # Install Congress devstack integration
 source ./pre_test_hook_common.sh
 CONGRESS_BASE=/opt/stack/new/congress/contrib/devstack
-cp $CONGRESS_BASE/lib/* $DEVSTACK_BASE/lib
-cp $CONGRESS_BASE/extras.d/* $DEVSTACK_BASE/extras.d
 
