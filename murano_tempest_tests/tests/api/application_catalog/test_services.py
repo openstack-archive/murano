06b5d0438f5614a45b21aa920214c2b3ac9106e2
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2016-01-30 02:07:46 +0300

    Add service management tests suite to the tempest plugin
    - Add service management tests to the tempest plugin
    - Separate positive and negative checks
    - Update base client class with service management methods
    - Refactor test implementation and improve code quality
    
    Change-Id: I9989fb519725e4c67da5d40c07d7ca7693c205f4
    Targets: blueprint migrate-to-tempest-plugin

---
+++ murano_tempest_tests/tests/api/application_catalog/test_services.py
@@ -0,0 +1,93 @@
+#    Copyright (c) 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.test import attr
+
+from murano_tempest_tests.tests.api.application_catalog import base
+from murano_tempest_tests import utils
+
+
+class TestServices(base.BaseApplicationCatalogTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestServices, cls).resource_setup()
+        name = utils.generate_name(cls.__name__)
+        cls.environment = cls.application_catalog_client.\
+            create_environment(name)
+
+    @classmethod
+    def resource_cleanup(cls):
+        cls.application_catalog_client.\
+            delete_environment(cls.environment['id'])
+        super(TestServices, cls).resource_cleanup()
+
+    @attr(type='smoke')
+    def test_get_services_list(self):
+        session = self.application_catalog_client.\
+            create_session(self.environment['id'])
+        self.addCleanup(self.application_catalog_client.delete_session,
+                        self.environment['id'], session['id'])
+        services_list = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        self.assertIsInstance(services_list, list)
+
+    @attr(type='smoke')
+    def test_create_and_delete_demo_service(self):
+        session = self.application_catalog_client.\
+            create_session(self.environment['id'])
+        self.addCleanup(self.application_catalog_client.delete_session,
+                        self.environment['id'], session['id'])
+        services_list = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        post_body = self._get_demo_app()
+        service = self.application_catalog_client.\
+            create_service(self.environment['id'], session['id'], post_body)
+        services_list_ = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        self.assertEqual(len(services_list) + 1, len(services_list_))
+        self.application_catalog_client.\
+            delete_service(self.environment['id'],
+                           session['id'],
+                           service['?']['id'])
+        services_list_ = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        self.assertEqual(len(services_list), len(services_list_))
+
+    @attr(type='smoke')
+    def test_get_service(self):
+        session = self.application_catalog_client.\
+            create_session(self.environment['id'])
+        self.addCleanup(self.application_catalog_client.delete_session,
+                        self.environment['id'], session['id'])
+        services_list = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        post_body = self._get_demo_app()
+        service = self.application_catalog_client.\
+            create_service(self.environment['id'], session['id'], post_body)
+        services_list_ = self.application_catalog_client.\
+            get_services_list(self.environment['id'], session['id'])
+        self.assertEqual(len(services_list) + 1, len(services_list_))
+        service_ = self.application_catalog_client.\
+            get_service(self.environment['id'],
+                        service['?']['id'],
+                        session['id'])
+        self.assertEqual(service, service_)
+
+    @attr(type='smoke')
+    def test_get_services_without_sess_id(self):
+        services = self.application_catalog_client.\
+            get_services_list(self.environment['id'], None)
+        self.assertIsInstance(services, list)
+        self.assertEqual([], services)
