538ab2362baed98b25a33c6d6490329d59bdb079
Kirill Zaitsev <k.zaitsev@me.com>
2016-06-07 02:49:11 +0300

    Pin tempest version for stable/liberty to 12.0.0
    Since murano tempest tests use unstable tempest code, updates to tempest
    can (and do) break murano's tests. This commit isolates tempest tests in
    a virtualenv environment and pins version of tempest, that murano run's
    the tests against to 12.0.0
    
    This commit unlocks gate-murano-devstack-dsvm
    
    Closes-Bug: #1586912
    
    Change-Id: Ib9734b0a21b2e6b14e79249418feff7dbd78d2c3

--- functionaltests/run_tests.sh
+++ functionaltests/run_tests.sh
@@ -3,13 +3,19 @@
 source ./run_tests_common.sh
 
 # Using .venv for tempest installation
-pushd $TEMPEST_DIR
+pushd ..
 python tools/install_venv.py
 source .venv/bin/activate
+# force compatible tempest version
 pip install nose
+pushd $TEMPEST_DIR
+git checkout 12.0.0
+pip install -r requirements.txt
+pip install -r test-requirements.txt
 nosetests -sv /opt/stack/new/murano/murano/tests/functional/api/v1
 RETVAL=$?
 deactivate
+popd
 popd
 exit $RETVAL
 
